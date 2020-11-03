80f68bacf955f1e78eab6fa4d3fc7ec171593be0
carolinamaciel1 <carolinamaciel37@gmail.com>
2020-11-03 18:01:54 -0300

    GET CEP working

---
+++ app/src/main/java/com/example/cepapiconsumer/CEP.java
@@ -0,0 +1,73 @@
+package com.example.cepapiconsumer;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CEP {
+    @NotNull
+    @Override
+    public String toString() {
+        return "CEP{" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+}
--- app/src/main/java/com/example/cepapiconsumer/MainActivity.kt
+++ app/src/main/java/com/example/cepapiconsumer/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.cepapiconsumer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.ExecutionException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnBuscarCep: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnBuscarCep = findViewById(R.id.btnMain_buscarCep)
+        btnBuscarCep.setOnClickListener { buscarCep() }
+    }
+
+    private fun buscarCep() {
+
+        lateinit var cep: EditText
+        lateinit var resposta: TextView
+
+        cep = findViewById(R.id.etMain_cep)
+        resposta = findViewById(R.id.etMain_resposta)
+
+        try {
+            val retorno = HttpService(cep.getText().toString()).execute().get()
+            if (retorno != null) {
+                resposta.setText(retorno.toString())
+            } else {
+                Toast.makeText(this, "Um erro ocorreu!", Toast.LENGTH_LONG).show()
+            }
+
+
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        } catch (e: ExecutionException) {
+            e.printStackTrace()
+        }
     }
 }
